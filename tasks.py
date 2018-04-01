995b8242e65bc94551613bb4b530284ed7a1aa50
Steve Pike <me@stevepike.co.uk>
2018-04-01 12:38:48 +0100

    add task runner

--- tasks.py
+++ tasks.py
@@ -23,6 +23,40 @@ def clean_up_db():
 
 def process_tasks():
     """Process all the tasks."""
+    conn = sqlite3.connect('tasks.db')
+    curs = conn.cursor()
+    curs.execute(
+        "SELECT * FROM tasks WHERE status = 'waiting' "
+        "ORDER BY timestamp ASC"
+    )
+    rows = curs.fetchall()
+    for row in rows:
+        row = dict(zip([d[0] for d in curs.description], row))
+        log.info("Running for row %s", row)
+        with Genderifier(
+            spotify_token=row['token'],
+            db_file_path='genderify.db',
+        ) as genderifier:
+            genderifier.set_artists_batch_from_spotify_public_playlist(
+                username=row['username'], playlist_id=row['playlist_id']
+            )
+            genderifier.genderise_batch()
+            report = genderifier.get_report()
+            log.info("Done for row, report = %s", report)
+            curs.execute(
+                "UPDATE tasks (female, male, unknown, state) VALUES "
+                "(?, ?, ?, ?) WHERE id = ?",
+                (
+                    report['female'],
+                    report['male'],
+                    report['unknown'] + report['nonbinary'],
+                    'processed'
+                )
+            )
+            curs.commit()
+            log.info("Updated.")
+    conn.close()
+    log.info("Done, bye!")
 
 
 if __name__ == '__main__':
